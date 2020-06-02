3f9a1572dbac4202074c08e27478283694aa7382
cai <181424167@qq.com>
2020-06-02 22:45:44 +0800

    no message

--- maven.remove_no_jar_dir/Form1.Designer.cs
+++ maven.remove_no_jar_dir/Form1.Designer.cs
@@ -29,34 +29,100 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.btnReDownJar = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
             this.btnDefault = new System.Windows.Forms.Button();
             this.btn_setDir = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.mavenHomeDirText = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.btnClean = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.richTextBox3);
+            this.panel1.Controls.Add(this.btnReDownJar);
+            this.panel1.Controls.Add(this.btnSelect);
+            this.panel1.Controls.Add(this.btnSearch);
             this.panel1.Controls.Add(this.btnClean);
             this.panel1.Controls.Add(this.btnDefault);
             this.panel1.Controls.Add(this.btn_setDir);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.mavenHomeDirText);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(414, 372);
+            this.panel1.Size = new System.Drawing.Size(1038, 199);
             this.panel1.TabIndex = 0;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.richTextBox3.Location = new System.Drawing.Point(535, 0);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(503, 199);
+            this.richTextBox3.TabIndex = 9;
+            this.richTextBox3.Text = "";
+            // 
+            // btnReDownJar
+            // 
+            this.btnReDownJar.Location = new System.Drawing.Point(301, 98);
+            this.btnReDownJar.Name = "btnReDownJar";
+            this.btnReDownJar.Size = new System.Drawing.Size(224, 38);
+            this.btnReDownJar.TabIndex = 8;
+            this.btnReDownJar.Text = "重新下载所选jar";
+            this.btnReDownJar.UseVisualStyleBackColor = true;
+            this.btnReDownJar.Click += new System.EventHandler(this.btnReDownJar_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(301, 152);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(224, 37);
+            this.btnSelect.TabIndex = 7;
+            this.btnSelect.Text = "打开所选jar目录";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(15, 99);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(280, 37);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "查找所有无效的lastupdated文件";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(15, 152);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(280, 37);
+            this.btnClean.TabIndex = 5;
+            this.btnClean.Text = "删除所有无效的lastupdated文件";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
             // 
             // btnDefault
             // 
-            this.btnDefault.Location = new System.Drawing.Point(296, 11);
+            this.btnDefault.Location = new System.Drawing.Point(15, 43);
             this.btnDefault.Name = "btnDefault";
-            this.btnDefault.Size = new System.Drawing.Size(75, 23);
+            this.btnDefault.Size = new System.Drawing.Size(123, 37);
             this.btnDefault.TabIndex = 4;
             this.btnDefault.Text = "默认目录";
             this.btnDefault.UseVisualStyleBackColor = true;
@@ -64,9 +130,9 @@
             // 
             // btn_setDir
             // 
-            this.btn_setDir.Location = new System.Drawing.Point(215, 12);
+            this.btn_setDir.Location = new System.Drawing.Point(155, 43);
             this.btn_setDir.Name = "btn_setDir";
-            this.btn_setDir.Size = new System.Drawing.Size(75, 23);
+            this.btn_setDir.Size = new System.Drawing.Size(123, 37);
             this.btn_setDir.TabIndex = 3;
             this.btn_setDir.Text = "设置目录";
             this.btn_setDir.UseVisualStyleBackColor = true;
@@ -83,46 +149,93 @@
             // 
             // mavenHomeDirText
             // 
-            this.mavenHomeDirText.Location = new System.Drawing.Point(12, 43);
+            this.mavenHomeDirText.Location = new System.Drawing.Point(211, 12);
             this.mavenHomeDirText.Name = "mavenHomeDirText";
-            this.mavenHomeDirText.Size = new System.Drawing.Size(396, 25);
+            this.mavenHomeDirText.Size = new System.Drawing.Size(318, 25);
             this.mavenHomeDirText.TabIndex = 2;
-            this.mavenHomeDirText.Text = "D:\\User\\Maven\\repository";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(414, 0);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(302, 372);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
+            this.mavenHomeDirText.Text = "C:\\Users\\Administrator\\.m2\\repository";
             // 
             // folderBrowserDialog1
             // 
             this.folderBrowserDialog1.SelectedPath = "C:\\Users\\Administrator\\Desktop\\软件需求";
             // 
-            // btnClean
+            // tabControl1
             // 
-            this.btnClean.Location = new System.Drawing.Point(15, 315);
-            this.btnClean.Name = "btnClean";
-            this.btnClean.Size = new System.Drawing.Size(199, 45);
-            this.btnClean.TabIndex = 5;
-            this.btnClean.Text = "清理下载失败的jar包";
-            this.btnClean.UseVisualStyleBackColor = true;
-            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 199);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1038, 404);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1030, 375);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "查找结果";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1024, 369);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(871, 574);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "操作日志";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(865, 568);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 37);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "我的自定义目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(716, 372);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(1038, 603);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "小工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -130,13 +243,22 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TextBox mavenHomeDirText;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_setDir;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button btnDefault;
         private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnReDownJar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- maven.remove_no_jar_dir/Form1.cs
+++ maven.remove_no_jar_dir/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace maven.remove_no_jar_dir
 {
@@ -76,6 +79,12 @@ namespace maven.remove_no_jar_dir
 
         private void btnClean_Click(object sender, EventArgs e)
         {
+
+            if (MessageBox.Show("确认删除吗", "提示", MessageBoxButtons.OKCancel)
+                != DialogResult.OK)
+            {
+                return;
+            }
             string path = mavenHomeDirText.Text;
 
             if (!Directory.Exists(path))
@@ -99,16 +108,18 @@ namespace maven.remove_no_jar_dir
             SetEnabled(btnClean, false);
             string path = obj as string;
 
-            BindRichBox(richTextBox1, "开始清理下载失败的jar文件夹...\r\n",false);
+            BindRichBox(richTextBox2, "开始清理lastupdated文件...\r\n", false);
 
             //获取目录下的所有文件
 
             loopCleanDir(path);
 
+
+
             SetEnabled(btnClean, true);
         }
 
-        private void loopCleanDir(string path)
+        private void loopCleanDir(string path,bool isClean=true)
         {
            String[] childrenDirs = Directory.GetDirectories(path);
 
@@ -116,29 +127,382 @@ namespace maven.remove_no_jar_dir
             {
                 foreach(string dir in childrenDirs)
                 {
-                    loopCleanDir(dir);
+                    loopCleanDir(dir,isClean);
                 }
             }
             else
             {
                 String[] files = Directory.GetFiles(path);
 
-                bool hasJar =false;
                 foreach(String file in files)
                 {
                     string extension = Path.GetExtension(file);
-                    if (extension.ToLower() == ".jar")
+                    if (extension.ToLower() == ".lastupdated")
                     {
-                        hasJar = true;
-                        break;
+                        if (isClean)
+                        {
+                            File.Delete(file);
+                            BindRichBox(richTextBox2, file + "......已删");
+                        }
+                        else
+                        {
+                            BindRichBox(richTextBox1, file);
+                        }
                     }
                 }
-                if (!hasJar)
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string path = mavenHomeDirText.Text;
+
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("目录不存在");
+                return;
+            }
+
+            if (path.LastIndexOf("repository") < 0)
+            {
+                //防止误删
+                MessageBox.Show("不是有效的maven仓库目录");
+                return;
+            }
+
+            new Thread(RunThreadSearchNoJarDir).Start(path);
+        }
+        private void RunThreadSearchNoJarDir(object obj)
+        {
+            SetEnabled(btnSearch, false);
+            string path = obj as string;
+
+            BindRichBox(richTextBox1, "开始查找下载失败的jar文件夹...\r\n", false);
+
+            //获取目录下的所有文件
+
+            loopCleanDir(path,false);
+
+            SetEnabled(btnSearch, true);
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int begin = richTextBox1.GetFirstCharIndexOfCurrentLine();
+                string endString = richTextBox1.Text.Substring(begin);
+                string text = endString;
+                int rtIndex = endString.IndexOf("\n");
+
+                if (rtIndex >= 0)
                 {
-                    Directory.Delete(path,true);
-                    BindRichBox(richTextBox1, path + "......已删除...\r\n");
+                    text = endString.Substring(0, rtIndex);
+                }
+
+                DirectoryInfo info = new DirectoryInfo(text);
+                text = info.Parent.FullName;
+                if (Directory.Exists(text))
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", text);
                 }
             }
+            catch
+            {
+
+            }
+        }
+
+
+        private void richTextBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+
+                int begin = richTextBox1.GetFirstCharIndexOfCurrentLine();
+                string endString = richTextBox1.Text.Substring(begin);
+                string text = endString;
+                int rtIndex = endString.IndexOf("\n");
+
+                if (rtIndex == -1)
+                {
+                    rtIndex = endString.Length;
+                }
+                else
+                {
+                    text = endString.Substring(0, rtIndex);
+                }
+
+                richTextBox1.SelectionStart = begin;
+                richTextBox1.SelectionLength = rtIndex;
+
+                DirectoryInfo info = new DirectoryInfo(text);
+                text = info.Parent.FullName;
+                if (!Directory.Exists(text))
+                {
+                    return;
+                }
+
+
+                string str = text.Replace(mavenHomeDirText.Text, "");
+                string[] strs = str.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                if (strs.Length < 3)
+                {
+                    return;
+                }
+
+                int len = strs.Length;
+                string groupId = strs[0];
+                string artifactId = strs[len - 2];
+                string version = strs[len - 1];
+
+                if ("0123456789".IndexOf(version[0].ToString()) == -1)
+                {
+                    BindRichBox(richTextBox3, "所选jar不符合规则", false);
+                    return;
+                }
+
+
+                dependency dy = new dependency();
+                dy.groupId = groupId;
+                dy.artifactId = artifactId;
+                dy.version = version;
+                string xml = dependency.JsonToXmlString(dy);
+
+                BindRichBox(richTextBox3, xml, false);
+
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void btnReDownJar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int begin = richTextBox1.GetFirstCharIndexOfCurrentLine();
+                string endString = richTextBox1.Text.Substring(begin);
+                string text = endString;
+                int rtIndex = endString.IndexOf("\n");
+
+                if (rtIndex >= 0)
+                {
+                    text = endString.Substring(0, rtIndex);
+                }
+                new Thread(threadDownJar).Start(new String[] { text , mavenHomeDirText .Text});
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void threadDownJar(object obj)
+        {
+            try
+            {
+                SetEnabled(btnReDownJar, false);
+                string[] paths = obj as string[];
+
+                string path = paths[0];
+                string mavenHomeDir = paths[1];
+
+                DirectoryInfo info = new DirectoryInfo(path);
+                path = info.Parent.FullName;
+
+                if (!Directory.Exists(path))
+                {
+                    SetEnabled(btnReDownJar, true);
+                    return;
+                }
+
+
+                //mvn dependency:get -DremoteRepositories=http://repo1.maven.org/maven2/ -DgroupId=org.freemarker -DartifactId=freemarker -Dversion=2.3.29
+
+                string str = path.Replace(mavenHomeDir, "");
+
+
+                string[] strs = str.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                if (strs.Length < 3)
+                {
+                    BindRichBox(richTextBox3, "所选jar不符合规则", false);
+                }
+
+                int len = strs.Length;
+                string groupId = strs[0];
+                string artifactId = strs[len - 2];
+                string version = strs[len - 1];
+
+
+                if ("0123456789".IndexOf(version[0].ToString()) == -1)
+                {
+                    BindRichBox(richTextBox3, "所选jar不符合规则", false);
+                }
+
+                
+                for (int i = 1; i < len - 2; i++)
+                {
+                    groupId += "." + strs[i];
+                }
+
+                string cmdStr = string.Format(@"mvn dependency:get -DremoteRepositories={0} -DgroupId={1} -DartifactId={2} -Dversion={3}",
+                    "http://repo1.maven.org/maven2/", groupId, artifactId, version
+                    );
+
+                BindRichBox(richTextBox2, cmdStr + "\n", false);
+
+                ExecuteInCmd(cmdStr);
+
+                SetEnabled(btnReDownJar, true);
+            }
+            catch
+            {
+                SetEnabled(btnReDownJar, true);
+            }
+
+        }
+
+        /// <summary>
+        /// 执行内部命令（cmd.exe 中的命令）
+        /// </summary>
+        /// <param name="cmdline">命令行</param>
+        /// <returns>执行结果</returns>
+        public  void ExecuteInCmd(string cmdline)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+
+                process.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+                process.ErrorDataReceived += new DataReceivedEventHandler(p_ErrorDataReceived);
+
+                process.Start();
+                process.StandardInput.AutoFlush = true;
+                process.StandardInput.WriteLine(cmdline + "&exit");
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit();
+                process.Close();
+
+            }
+        }
+
+        private void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                // 4. 异步调用，需要invoke
+                this.Invoke(ReadStdOutput, new object[] { e.Data + "ss" + e.Data.Length });
+            }
+        }
+        private void p_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                this.Invoke(ReadErrOutput, new object[] { e.Data });
+            }
+        }
+
+        public delegate void DelReadStdOutput(string result);
+        public delegate void DelReadErrOutput(string result);
+
+        public event DelReadStdOutput ReadStdOutput;
+        public event DelReadErrOutput ReadErrOutput;
+
+        private  void ReadStdOutputAction(string result)
+        {
+            BindRichBox(richTextBox2, result , true);
+        }
+        private  void ReadErrOutputAction(string result)
+        {
+            BindRichBox(richTextBox2, result , true);
+        }
+
+
+        /// <summary>
+        /// 执行外部命令
+        /// </summary>
+        /// <param name="argument">命令参数</param>
+        /// <param name="application">命令程序路径</param>
+        /// <returns>执行结果</returns>
+        public static string ExecuteOutCmd(string argument, string applocaltion)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo.Arguments = argument;
+                process.StartInfo.FileName = applocaltion;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+                process.StandardInput.AutoFlush = true;
+                process.StandardInput.WriteLine("exit");
+
+                //获取cmd窗口的输出信息  
+                string output = process.StandardOutput.ReadToEnd();
+
+                process.WaitForExit();
+                process.Close();
+
+                return output;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //3.将相应函数注册到委托事件中
+            ReadStdOutput += new DelReadStdOutput(ReadStdOutputAction);
+            ReadErrOutput += new DelReadErrOutput(ReadErrOutputAction);
+        }
+
+
+
+        public class dependency
+        {
+            public string groupId { get; set; }
+            public string artifactId { get; set; }
+            public string version { get; set; }
+
+            public static string JsonToXmlString(dependency obj)
+            {
+                XmlDocument document = new XmlDocument();
+                StringBuilder sb = new StringBuilder(); string serialized = string.Empty;
+                using (TextWriter tw = new StringWriter(sb))
+                {
+                    var xmlS = new XmlSerializer(typeof(dependency));
+                    xmlS.Serialize(tw, obj);
+                    serialized = sb.ToString();
+                    serialized = serialized.Replace("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n", "");
+                    serialized = serialized.Replace(" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ", "");
+                    serialized = serialized.Replace("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"", "");
+                    
+                }
+
+                return serialized.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mavenHomeDirText.Text = @"D:\User\Maven\repository";
         }
     }
 }
